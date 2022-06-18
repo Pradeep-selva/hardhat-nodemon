420a9115f46823e7362a6af92de51e80094fc889
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-18 21:16:40 +0530

    ft: generically compile on every change

--- src/tasks/compile.ts
+++ src/tasks/compile.ts
@@ -3,12 +3,21 @@ import {task} from "hardhat/config";
 import {TASK_COMPILE} from "hardhat/builtin-tasks/task-names";
 import {command} from "../config";
 
+const showWatching = () => console.log("[Watching for contract changes...]");
+
 task(`${TASK_COMPILE}${command.watch}`).setAction(async (_, hre) => {
+  const {compileDir} = hre.config.compileWatch;
+
   await hre.run(TASK_COMPILE);
 
-  console.log("[Watching for contract changes...]");
+  showWatching();
 
-  const watcher = watch(hre.config.compileWatch.compileDir);
-  for await (const event of watcher)
-    console.log(`${event.filename} has been ${event.eventType}.`);
+  const watcher = watch(compileDir);
+  for await (const event of watcher) {
+    console.log(`${event.filename} -> ${event.eventType}`);
+
+    await hre.run(`${TASK_COMPILE}`);
+
+    showWatching();
+  }
 });
