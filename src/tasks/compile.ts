4035cb4fe0272875c373e2b6365d8c76930b1c2e
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-18 22:46:45 +0530

    ft: flags and config checks

--- src/tasks/compile.ts
+++ src/tasks/compile.ts
@@ -4,19 +4,30 @@ import {TASK_COMPILE} from "hardhat/builtin-tasks/task-names";
 import {command} from "../config";
 import {showChange, showWatching} from "../utils";
 
-task(`${TASK_COMPILE}${command.watch}`).setAction(async (_, hre) => {
-  const {compileDir} = hre.config.compileWatch;
+task(`${TASK_COMPILE}${command.watch}`)
+  .addFlag(
+    "only",
+    "A list of contracts to watch for compilation, separated by commas (with extension)",
+  )
+  .addFlag(
+    "except",
+    "A list of contracts to ignore while watching for compilation, separated by commas (with extension)",
+  )
+  .setAction(async (_, hre) => {
+    const {compileDir, noCompile} = hre.config.compileWatch;
 
-  await hre.run(TASK_COMPILE);
-
-  showWatching();
-
-  const watcher = watch(compileDir, {recursive: true});
-  for await (const {filename, eventType} of watcher) {
-    showChange(filename, eventType);
-
-    await hre.run(`${TASK_COMPILE}`);
+    if (!noCompile) {
+      await hre.run(TASK_COMPILE);
+    }
 
     showWatching();
-  }
-});
+
+    const watcher = watch(compileDir, {recursive: true});
+    for await (const {filename, eventType} of watcher) {
+      showChange(filename, eventType);
+
+      await hre.run(`${TASK_COMPILE}`);
+
+      showWatching();
+    }
+  });
