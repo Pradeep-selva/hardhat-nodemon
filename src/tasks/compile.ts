4a9183ab0cbc7fbf32b7d2209f95959798d43003
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-18 21:28:25 +0530

    ft: better outputs

--- src/tasks/compile.ts
+++ src/tasks/compile.ts
@@ -2,8 +2,7 @@ import {watch} from "fs/promises";
 import {task} from "hardhat/config";
 import {TASK_COMPILE} from "hardhat/builtin-tasks/task-names";
 import {command} from "../config";
-
-const showWatching = () => console.log("[Watching for contract changes...]");
+import {showChange, showWatching} from "../utils";
 
 task(`${TASK_COMPILE}${command.watch}`).setAction(async (_, hre) => {
   const {compileDir} = hre.config.compileWatch;
@@ -13,8 +12,8 @@ task(`${TASK_COMPILE}${command.watch}`).setAction(async (_, hre) => {
   showWatching();
 
   const watcher = watch(compileDir);
-  for await (const event of watcher) {
-    console.log(`${event.filename} -> ${event.eventType}`);
+  for await (const {filename, eventType} of watcher) {
+    showChange(filename, eventType);
 
     await hre.run(`${TASK_COMPILE}`);
 
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+export const showWatching = () =>
+  console.log(chalk.green.bold("\n[Watching for contract changes...]\n"));
+
+export const showChange = (fileName: string, event: string) =>
+  console.log(
+    `[${chalk.yellow.bold(fileName)}] ${chalk.green(event.toUpperCase())}`,
+  );
